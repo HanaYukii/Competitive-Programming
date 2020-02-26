e8cf03c3a8873017fbb67da1b154b937bccdef4f
HanaYukii <islu245777@gmail.com>
2020-02-26 11:36:24 +0800

    Create CF718C.cpp

---
+++ topic-train/Segment Tree/CF718C.cpp
@@ -0,0 +1,137 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define pb push_back
+#define ll long long
+#define fr(i,j,k) for(int i=j;i<k;i++)
+#define f(n) fr(i,0,n)
+#define f1(n) fr(i,1,n+1)
+#define ms(i) memset(i,0,sizeof(i));
+#define ms1(i) memset(i,-1,sizeof(i));
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+#define F first
+#define S second
+const int maxn = 1e5 + 5;
+const int mod = 1e9 + 7;
+
+struct mat {
+    ll a[2][2];
+    mat() {
+        memset( a, 0, sizeof( a ) );
+    }
+    mat operator * ( const mat &b ) const {
+        mat ret;
+        for( int i = 0; i < 2; ++i )
+            for( int j = 0; j < 2; ++j )
+                for( int k = 0; k < 2; ++k )
+                    ret.a[i][j] = ( ret.a[i][j] + a[i][k] * b.a[k][j] ) % mod;
+        return ret;
+    }
+    mat operator + ( const mat &b ) const {
+        mat ret;
+        for( int i = 0; i < 2; ++i )
+            for( int j = 0; j < 2; ++j )
+                ret.a[i][j] = (a[i][j] + b.a[i][j] ) % mod;
+        return ret;
+    }
+}sum[maxn<<2], lz[maxn<<2];
+
+mat get(int k) {
+    mat ret;
+    mat sum;
+    sum.a[0][0] = 0, sum.a[0][1] = 1;
+    sum.a[1][0] = 1, sum.a[1][1] = 1;
+    ret.a[0][0] = ret.a[1][1] = 1;
+    while (k) {
+        if (k & 1) {
+            ret = ret * sum;
+        }
+        sum = sum * sum;
+        k >>= 1;
+    }
+    return ret;
+}
+void init(int x) {
+    lz[x].a[0][0] = 1, lz[x].a[0][1] = 0;
+    lz[x].a[1][0] = 0, lz[x].a[1][1] = 1;
+}
+
+void pull(int x) {
+    sum[x] = sum[x<<1|1] + sum[x<<1];
+}
+void push(int x) {
+    sum[x<<1] = sum[x<<1] * lz[x];
+    sum[x<<1|1] = sum[x<<1|1] * lz[x];
+    lz[x<<1] = lz[x<<1] * lz[x];
+    lz[x<<1|1] = lz[x<<1|1] * lz[x];
+    init(x);
+}
+void build( int x, int l, int r ) {
+    init(x);
+    if(l == r) {
+        sum[x].a[0][0] = 0;
+        sum[x].a[0][1] = 1;
+        int idx;
+        cin >> idx;
+        sum[x] = sum[x] * get(idx);
+        return;
+    }
+    int mid = (l + r) >> 1;
+    build(x<<1,l,mid);
+    build(x<<1|1,mid+1,r);
+    pull(x);
+}
+ll query( int x, int l, int r, int ql,int qr ) {
+    if(ql <= l && qr >= r) {
+        return sum[x].a[0][0];
+    }
+    int mid = (l + r) >> 1;
+    push(x);
+    if(qr <= mid) {
+        return query(x<<1,l,mid,ql,qr);
+    } 
+    else if(ql > mid) {
+        return query(x<<1|1,mid+1,r,ql,qr);
+    }
+    else {
+        return (query(x<<1|1,mid+1,r,ql,qr) + query(x<<1,l,mid,ql,qr)) % mod;
+    }
+}
+void update(int x,int l,int r,int ql,int qr, mat m) {
+    if(ql <= l && qr >= r) {
+        sum[x] = sum[x] * m;
+        lz[x] = lz[x] * m;
+        return;
+    }
+    push(x);
+    int mid = (l + r) >> 1;
+    if(ql <= mid) {
+        update(x<<1,l,mid,ql,qr,m);
+    }
+    if(qr > mid) {
+        update(x<<1|1,mid+1,r,ql,qr,m);
+    }
+    pull(x);
+}
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n, m;
+        cin >> n >> m;
+        build(1,1,n);
+        while(m --) {
+            int tp, l, r;
+            cin >> tp >> l >> r;
+            if(tp == 1) {
+                int k;
+                cin >> k;
+                mat add = get(k);
+                update(1,1,n,l,r,add);
+            }
+            else {
+                cout << query(1,1,n,l,r) << '\n';
+            }
+        }
+    
+}
