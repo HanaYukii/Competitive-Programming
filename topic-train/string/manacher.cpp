6ca7f385ccd735abda37d1559bb2b9af202be152
HanaYukii <islu245777@gmail.com>
2020-04-09 15:36:54 +0800

    Update manacher.cpp

--- topic-train/string/manacher.cpp
+++ topic-train/string/manacher.cpp
@@ -7,29 +7,33 @@ int main(){
     char s[225000];
     while(~scanf("%s",ss)){
         int n = strlen(ss);
-        s[0] = '*';
+        // 填充字串
+        s[0] = '#';
         for(int i = 0 ; i < n ; i++){
             s[i*2+1] = '#';
             s[i*2+2] = ss[i];
         }
         s[n * 2 + 1] = '#';
-        int mxr = 0,mxp = 0;
+        int mxr = 0, mxp = 0; // 目前最右點，最右點的中點
         int ans = 0;
         n = n * 2 + 2;
         int dp[n + 5] = {};
-        for(int i = 1 ; i < n ; i++){
-            if(i < mxr){
+        for(int i = 1 ; i < n ; i++){ //左至右枚舉所有中點
+            if(i <= mxr){ // 此位置目前在先前找到的對稱子字串中，有對應的PART
                 dp[i] = min(mxr - i,dp[2 * mxp - i]);
             }
             else{
-                dp[i] = 1;
+                dp[i] = 0;
             }
-            while(s[i-dp[i]] == s[i+dp[i]])dp[i]++;
-            if( i + dp[i] > mxr){
+            if (i + dp[i] >= mxr){
+               while(s[i-dp[i]] == s[i+dp[i]])dp[i]++; // 暴力比對
+               dp[i]--;
+            }
+            if( i + dp[i] > mxr){ // 此位置為中心 找到更右的對稱子字串
                 mxr = i + dp[i];
                 mxp = i;
             }
-            ans = max(ans,dp[i] - 1);
+            ans = max(ans,dp[i]);
         }
         printf("%d\n",ans);
     }
