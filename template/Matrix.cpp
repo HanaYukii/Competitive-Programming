07ccde05b97b47d66a38c5dc7e28a41e39c53299
HanaYukii <islu245777@gmail.com>
2020-06-24 19:01:27 +0800

    Update Matrix.cpp

--- template/Matrix.cpp
+++ template/Matrix.cpp
@@ -1,14 +1,34 @@
 struct mat {
-    ll a[2][2];
+    ll a[6][6];
     mat() {
         memset(a, 0, sizeof(a));
     }
     mat operator * ( const mat &b ) const {
         mat ret;
-        for( int i = 0; i < 2; ++i )
-            for( int j = 0; j < 2; ++j )
-                for( int k = 0; k < 2; ++k )
+        for( int i = 0; i < 6; ++i )
+            for( int j = 0; j < 6; ++j )
+                for( int k = 0; k < 6; ++k )
                     ret.a[i][j] = ( ret.a[i][j] + a[i][k] * b.a[k][j] ) % mod;
         return ret;
     }
 };
+void solve() {
+    ll n;
+    mat ans, now;
+    cin >> n;
+    f(5) {
+        now.a[i][i + 1] = 1;
+    }
+    f(6) {
+        now.a[5][i] = 1;
+    }
+    ans.a[5][0] = 1;
+    while (n) {
+        if (n & 1) {
+            ans = now * ans;
+        }
+        now = now * now;
+        n >>= 1;
+    }
+    cout << ans.a[5][0] << '\n';
+}
