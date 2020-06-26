db4ff46b28b4848de6c21bb1f6ba85fc4e1edf7b
HanaYukii <islu245777@gmail.com>
2020-06-27 00:31:04 +0800

    Create KMP.cpp

---
+++ template/KMP.cpp
@@ -0,0 +1,37 @@
+vector<int>fail;
+void build(string s) {
+    fail.pb(-1);
+    int p1 = 0, p2 = -1;
+    while (p1 < (int)s.size()) {
+        if (p2 == -1 || s[p1] == s[p2]) {
+            p1++, p2++;
+            fail.pb(p2);
+        }
+        else {
+            p2 = fail[p2];
+        }
+    }
+}
+int match(string s, string t) {
+    int p1 = 0, p2 = 0;
+    int ans = 0;
+    while (p1 < (int)s.size()) {
+        if (p2 == -1 || s[p1] == t[p2]) {
+            p1++, p2++;
+            if (p2 == (int)t.size()) {
+                ans++;
+                p2 = fail[p2];
+            }
+        }
+        else {
+            p2 = fail[p2];
+        }
+    }
+    return ans;
+}
+void solve() {
+    string s, t;
+    cin >> s >> t;
+    build(t);
+    cout << match(s, t) << '\n';
+}
