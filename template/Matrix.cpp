b02a4e4c772e74db1faa139b5dc96ec9ef42ef2b
HanaYukii <islu245777@gmail.com>
2020-09-14 16:05:19 +0800

    Update Matrix.cpp

--- template/Matrix.cpp
+++ template/Matrix.cpp
@@ -1,34 +1,23 @@
 struct mat {
-    ll a[6][6];
+    ll a[100][100];
+    int x;
     mat() {
         memset(a, 0, sizeof(a));
     }
     mat operator * ( const mat &b ) const {
         mat ret;
-        for( int i = 0; i < 6; ++i )
-            for( int j = 0; j < 6; ++j )
-                for( int k = 0; k < 6; ++k )
+        for( int i = 0; i < x; ++i )
+            for( int j = 0; j < x; ++j )
+                for( int k = 0; k < x; ++k )
+                    ret.a[i][j] = ( ret.a[i][j] + a[i][k] * b.a[k][j] ) % mod;
+        return ret;
+    }
+    mat operator + ( const mat &b ) const {
+        mat ret;
+        for( int i = 0; i < x; ++i )
+            for( int j = 0; j < 1; ++j )
+                for( int k = 0; k < x; ++k )
                     ret.a[i][j] = ( ret.a[i][j] + a[i][k] * b.a[k][j] ) % mod;
         return ret;
     }
 };
-void solve() {
-    ll n;
-    mat ans, now;
-    cin >> n;
-    f(5) {
-        now.a[i][i + 1] = 1;
-    }
-    f(6) {
-        now.a[5][i] = 1;
-    }
-    ans.a[5][0] = 1;
-    while (n) {
-        if (n & 1) {
-            ans = now * ans;
-        }
-        now = now * now;
-        n >>= 1;
-    }
-    cout << ans.a[5][0] << '\n';
-}
